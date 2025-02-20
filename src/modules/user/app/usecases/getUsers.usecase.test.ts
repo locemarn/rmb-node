44f5e8a514491bc5ae79f2f286fdb85845189208
Marcelo Nogueira <marcelon27@gmail.com>
2025-02-20 15:26:46 -0300

    Test: Adding test to get all users use case

--- src/modules/user/app/usecases/getUsers.usecase.test.ts
+++ src/modules/user/app/usecases/getUsers.usecase.test.ts
@@ -1,5 +1,24 @@
-describe('should get users', () => {
-  it('should return 200 OK with users', () => {
-    expect(true).toBeTruthy()
+import { UserRepositoryInterface } from '../../domain/repository/userRepository.interface'
+import { MockUserRepository } from '../../infra/repository/mockUsers.repository'
+import { GetUsersUseCase } from './getUsers.usecase'
+
+describe('GetUsersUseCase', () => {
+  let repo: UserRepositoryInterface
+
+  beforeEach(() => {
+    repo = new MockUserRepository()
+  })
+
+  afterEach(() => {
+    repo = {} as MockUserRepository
+    jest.clearAllMocks()
+  })
+
+  test('should return all users', async () => {
+    const useCase = new GetUsersUseCase(repo)
+    const users = await useCase.execute()
+
+    expect(users).toBeInstanceOf(Array)
+    expect(users.length).toBeGreaterThan(0)
   })
 })
