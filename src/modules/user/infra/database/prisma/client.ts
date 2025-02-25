2e2b2d859dc98a7a130d77568cf67de6f241ea7a
Marcelo Nogueira <marcelon27@gmail.com>
2025-02-25 15:58:59 -0300

    Config: Updating Prisma client connection

--- src/modules/user/infra/database/prisma/client.ts
+++ src/modules/user/infra/database/prisma/client.ts
@@ -1,28 +1,13 @@
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import config from '../../../../../config'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = config.db.db_url
 
-const pool: Pool = new Pool({
-  connectionString,
-  max: 5,
-  idleTimeoutMillis: 5000,
-})
-const adapter: PrismaPg = new PrismaPg(pool)
+const pool = new Pool({ connectionString })
+
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
-
-// Error handling
-prisma
-  .$connect()
-  .then(() => console.log('Successfully connected to the database'))
-  .catch((error) => console.error('Failed to connect to the database:', error))
-
-// Proper shutdown
-process.on('SIGINT', async () => {
-  await prisma.$disconnect()
-  await pool.end()
-  process.exit(0)
-})
 
 export default prisma
