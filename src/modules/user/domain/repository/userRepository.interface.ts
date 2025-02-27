1d2f417160880dfac3db17cabdd00faea254780a
Marcelo Nogueira <marcelon27@gmail.com>
2025-02-27 15:31:41 -0300

    Feat: configuring all users returns without passw

--- src/modules/user/domain/repository/userRepository.interface.ts
+++ src/modules/user/domain/repository/userRepository.interface.ts
@@ -1,7 +1,7 @@
 import { UserEntity } from '../entity/user.entity'
 
 export interface UserRepositoryInterface {
-  getUsers(): Promise<UserEntity[]>
+  getUsers(): Promise<Omit<UserEntity, 'password'>[]>
   create(user: UserEntity): Promise<UserEntity>
   update(id: number, user: UserEntity): Promise<UserEntity | null>
   delete(id: number): Promise<UserEntity | null>
